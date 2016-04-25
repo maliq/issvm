09430240d7058785d6bddfded1b9ce0f8d4684ad
maliq <mmaliq@gmail.com>
2016-04-25 18:19:43 -0300

    added to bash test different tolerances and save in files

--- experiments/base_sparsifier.sh
+++ experiments/base_sparsifier.sh
@@ -117,3 +117,48 @@ if [ "$1" == "test" ]; then
 	done
 fi
 
+
+if [ "$1" == "ttol" ]; then
+	if [ "$2" != "" ]; then
+		norms=(${TOLs[${2}]})
+	fi
+
+    for TOL in "${TOLs[@]}"
+	    do
+        for NORM in "${norms[@]}"
+        do
+        #NORM=${norms[${2}]}
+        sum_test_error=0
+        sum_sv=0
+        for ((i=1;i<=10;i++)); do
+            BEST_ERROR=1
+                for ETA in "${etas[@]}"
+                do
+                    #echo "issvm_evaluate -f $dataset_dir/$TRAIN_DATA -i $MODEL_DIR/${DATASET}_SVM_${METHOD}_NORM-${NORM}_ETA-${ETA}_EP-${EPSILON}_BIASED_${ITERATIONS} -o $TEST_DIR/${DATASET}_SVM_${METHOD}_NORM-${NORM}_ETA-${ETA}_EP-${EPSILON}_BIASED_${ITERATIONS}_train.predited"
+                    OUTPUT="$(issvm_test -f $dataset_dir/${VAL_DATA}${i} -i $MODEL_DIR/${DATASET}_SVM_${METHOD}_NORM-${NORM}_ETA-${ETA}_EP-${EPSILON}_BIASED_${TOL})"
+                    #echo $OUTPUT
+                    arrIN=(${OUTPUT})
+                    #echo ${arrIN[2]} $ETA
+                    echo "${NORM};${ETA};${i};${arrIN[1]};${arrIN[2]}" >> ${METHOD}/${DATASET}_${TOL}_validation.txt
+                    if (( $(bc <<< "${arrIN[2]} < $BEST_ERROR") ))
+                    then
+                        BEST_ERROR=${arrIN[2]}
+                        BEST_ETA=$ETA
+                        BEST_TOL=$TOL
+                    fi
+                done
+
+            OUTPUT="$(issvm_test -f $dataset_dir/${TEST_DATA}${i} -i $MODEL_DIR/${DATASET}_SVM_${METHOD}_NORM-${NORM}_ETA-${BEST_ETA}_EP-${EPSILON}_BIASED_${BEST_TOL})"
+            arrIN=(${OUTPUT})
+            test_error=${arrIN[2]}
+            #echo "${arrIN[1]} $test_error ${BEST_ETA} ${BEST_TOL}"
+            sum_test_error=$(echo "scale=5; $test_error+$sum_test_error" | bc -l)
+            sum_sv=$(echo "${arrIN[1]}+$sum_sv" | bc -l)
+        done
+        mean_sv=$(echo "scale=1; ${sum_sv}/10" | bc -l)
+        mean_test_error=$(echo "scale=5; ${sum_test_error}/10" | bc -l)
+        #echo $mean_sv $mean_test_error "0 0"
+        echo "${NORM};${mean_sv};${mean_test_error}" >> ${DATASET}_${TOL}_test.txt
+        done
+    done
+fi
